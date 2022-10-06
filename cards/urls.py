6cd682a7a4f4d3f4f50906f890d173afb3b62c3e
cristianvitor01 <cristianvitoralves@gmail.com>
2022-10-06 11:05:29 -0300

    add files

---
+++ cards/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# from django.views.generic import TemplateView
+
+urlpatterns = [
+
+    path("", views.CardListView.as_view(), name="card-list"),
+    path("new", views.CardCreateView.as_view(), name="card-create"),
+    path("edit/<int:pk>", views.CardUpdateView.as_view(), name="card-update"),
+    path("box/<int:box_num>", views.BoxView.as_view(), name="box"),
+]
